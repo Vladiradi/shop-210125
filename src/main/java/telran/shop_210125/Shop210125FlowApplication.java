df9331396a25d0d30c371c7396665fca1bb756a6
Vladiradi <rzhvlad@gmail.com>
2025-05-04 23:27:46 +0200

    make shop branch

---
+++ src/main/java/telran/shop_210125/Shop210125FlowApplication.java
@@ -0,0 +1,13 @@
+package telran.shop_210125;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shop210125FlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Shop210125FlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/telran/shop_210125/Shop210125FlowApplicationTests.java
@@ -0,0 +1,13 @@
+package telran.shop_210125;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shop210125FlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
